5555e30ff3a3ff6a0a1b53089f1294e6cec40894
Dzenny <batterflyfly96@gmail.com>
2026-08-08 17:36:42 +0300

    Modularize index.html and fix menu arrows

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
+import injectHTML from "vite-plugin-html-inject";
 
 // Relative base so assets work on GitHub Pages project URL:
 // https://USERNAME.github.io/REPO_NAME/
 export default defineConfig({
   base: "./",
+  plugins: [injectHTML()],
 });
